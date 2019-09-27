0eb9c5403fb58e00c8134c61eb6656f4c3dcc02c
Jeremy Kahn <jeremyckahn@gmail.com>
2019-09-26 21:07:47 -0500

    Store trimmedLines

--- test/index.js
+++ test/index.js
@@ -4,16 +4,59 @@ import assert from 'assert';
 
 import { readFileSync } from 'fs';
 
+/* eslint-disable no-unused-vars */
 const fixture = readFileSync(path.join(__dirname, 'fixtures/moon.stl'), 'utf8');
+/* eslint-enable no-unused-vars */
+
+const simpleExample = `
+solid simple
+  facet normal 0 0 0
+      outer loop
+          vertex 0 0 0
+          vertex 1 0 0
+          vertex 1 1 1
+      endloop
+  endfacet
+  facet normal 0 0 0
+      outer loop
+          vertex 0 0 0
+          vertex 0 1 1
+          vertex 1 1 1
+      endloop
+  endfacet
+endsolid simple
+`;
 
 describe('stl-parser', () => {
   let parser;
 
   before(() => {
-    parser = new StlParser(fixture);
+    parser = new StlParser(simpleExample);
   });
 
   it('stores raw data', () => {
-    assert.equal(parser.data, fixture);
+    assert.equal(parser.data, simpleExample);
   });
+
+  // it('stores trimmed data', () => {
+  // assert.equal(
+  // parser.trimmedLines,
+  // `solid simple
+  // facet normal 0 0 0
+  // outer loop
+  // vertex 0 0 0
+  // vertex 1 0 0
+  // vertex 1 1 1
+  // endloop
+  // endfacet
+  // facet normal 0 0 0
+  // outer loop
+  // vertex 0 0 0
+  // vertex 0 1 1
+  // vertex 1 1 1
+  // endloop
+  // endfacet
+  // endsolid simple`
+  // );
+  // });
 });
